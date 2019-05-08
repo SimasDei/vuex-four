acb24407530fc401429918b0603a8d0b94c4c2f5
Simas <simonas@digitouch.lt>
2019-05-08 09:12:47 +0300

    add getters, integrate babel stage-2

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -1,53 +1,62 @@
 <template>
-    <div id="registration">
-        <h3>Register here</h3>
-        <hr>
-        <div class="row" v-for="user in users">
-            <h4>{{ user.name }}</h4>
-            <button @click="registerUser(user)">Register</button>
-        </div>
+  <div id="registration">
+    <h3>Register here</h3>
+    <hr>
+    <div class="row" v-for="user in users" :key="user.id">
+      <h4>{{ user.name }}</h4>
+      <button @click="registerUser(user)">Register</button>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: ['users'],
-        methods: {
-            registerUser(user) {
-                this.$emit('userRegistered', user);
-                user.registered = true;
-            }
-        }
+export default {
+  computed: {
+    users() {
+      return this.$store.getters.unregisteredUsers;
     }
+  },
+  methods: {
+    registerUser(user) {
+      const date = new Date();
+      user.registered = true;
+      this.$store.state.registrations.push({
+        userId: user.id,
+        name: user.name,
+        date: date.getMonth() + "/" + date.getDay()
+      });
+    }
+  }
+};
 </script>
 
 <style scoped>
-    #registration {
-        box-shadow: 1px 1px 2px 1px #ccc;
-        margin: 20px;
-        padding: 20px;
-        display: inline-block;
-        width: 300px;
-        vertical-align: top;
-    }
+#registration {
+  box-shadow: 1px 1px 2px 1px #ccc;
+  margin: 20px;
+  padding: 20px;
+  display: inline-block;
+  width: 300px;
+  vertical-align: top;
+}
 
-    .row h4 {
-        display: inline-block;
-        width: 70%;
-        text-align: left;
-        margin: 0 0 10px 0;
-    }
+.row h4 {
+  display: inline-block;
+  width: 70%;
+  text-align: left;
+  margin: 0 0 10px 0;
+}
 
-    button {
-        background-color: lightgreen;
-        border: none;
-        box-shadow: 1px 1px 1px black;
-        font-size: inherit;
-        text-align: right;
-        cursor: pointer;
-    }
+button {
+  background-color: lightgreen;
+  border: none;
+  box-shadow: 1px 1px 1px black;
+  font-size: inherit;
+  text-align: right;
+  cursor: pointer;
+}
 
-    button:hover {
-        background-color: green;
-    }
+button:hover {
+  background-color: green;
+}
 </style>
--- src/components/Registrations.vue
+++ src/components/Registrations.vue
@@ -1,70 +1,78 @@
 <template>
-    <div id="registrations">
-        <div class="summary">
-            <h3>Registrations</h3>
-            <h5>Total: {{ total }}</h5>
-        </div>
-        <hr>
-        <div class="row" v-for="registration in registrations">
-            <h4>{{ registration.name }}</h4>
-            <span @click="unregister(registration)">(Unregister)</span>
-            <div class="date">{{ registration.date }}</div>
-        </div>
+  <div id="registrations">
+    <div class="summary">
+      <h3>Registrations</h3>
+      <h5>Total: {{ total }}</h5>
     </div>
+    <hr>
+    <div class="row" v-for="registration in registrations" :key="registration.userId">
+      <h4>{{ registration.name }}</h4>
+      <span @click="unregister(registration)">(Unregister)</span>
+      <div class="date">{{ registration.date }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: ['registrations'],
-        methods: {
-            unregister(registration) {
-                this.$emit('userUnregistered', registration);
-            }
-        },
-        computed: {
-            total() {
-                return this.registrations.length;
-            }
-        }
+import { mapGetters } from "vuex";
+export default {
+  methods: {
+    unregister(registration) {
+      const user = this.$store.state.users.find(user => {
+        return user.id == registration.userId;
+      });
+      user.registered = false;
+      this.$store.state.registrations.splice(
+        this.$store.state.registrations.indexOf(registration),
+        1
+      );
     }
+  },
+  computed: {
+    ...mapGetters({
+      registrations: "registrations",
+      total: "totalRegistrations"
+    })
+  }
+};
 </script>
 
 <style scoped>
-    #registrations {
-        box-shadow: 1px 1px 2px 1px #ccc;
-        margin: 20px;
-        padding: 20px;
-        display: inline-block;
-        width: 300px;
-        vertical-align: top;
-        text-align: left;
-    }
+#registrations {
+  box-shadow: 1px 1px 2px 1px #ccc;
+  margin: 20px;
+  padding: 20px;
+  display: inline-block;
+  width: 300px;
+  vertical-align: top;
+  text-align: left;
+}
 
-    .summary {
-        text-align: center;
-    }
+.summary {
+  text-align: center;
+}
 
-    .row h4 {
-        display: inline-block;
-        width: 30%;
-        margin: 0 0 10px 0;
-        box-sizing: border-box;
-    }
+.row h4 {
+  display: inline-block;
+  width: 30%;
+  margin: 0 0 10px 0;
+  box-sizing: border-box;
+}
 
-    .row span {
-        width: 30%;
-        color: red;
-        cursor: pointer;
-    }
+.row span {
+  width: 30%;
+  color: red;
+  cursor: pointer;
+}
 
-    .row span:hover {
-        color: darkred;
-    }
+.row span:hover {
+  color: darkred;
+}
 
-    .date {
-        display: inline-block;
-        width: 38%;
-        text-align: right;
-        box-sizing: border-box;
-    }
+.date {
+  display: inline-block;
+  width: 38%;
+  text-align: right;
+  box-sizing: border-box;
+}
 </style>
